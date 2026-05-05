74434ea0a4495fd54adfc4a7c9651bdb0d8ef6bd
André Maia <andre.r.maia@gmail.com>
2026-05-05 14:16:40 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,45 @@
 const venda = document.getElementById("venda");
 const aluguel = document.getElementById("aluguel");
+const temporada = document.getElementById("temporada");
 
 function formatar(valor) {
-  return valor.toLocaleString("pt-BR", {
-    style: "currency",
-    currency: "BRL"
-  });
+    return valor.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
 }
 
 function criarCard(imovel) {
-  const div = document.createElement("div");
-  div.className = "card";
+    const div = document.createElement("div");
+    div.className = "card";
 
-  div.innerHTML = `
-    <img src="${imovel.imagem}">
-    <div class="card-content">
-      <h3>${imovel.titulo}</h3>
-      <p>${imovel.descricao}</p>
-      <p><strong>${formatar(imovel.preco)}</strong></p>
-      <a class="botao" href="#">Saiba mais</a>
-    </div>
-  `;
+    div.innerHTML = `
+        <img src="${imovel.imagem}" alt="${imovel.titulo}">
+        <div class="card-content">
+            <h3>${imovel.titulo}</h3>
+            <p>${imovel.descricao}</p>
+            <strong>${formatar(imovel.preco)}</strong>
+            <a href="https://wa.me/5584987755048" class="botao">
+                Falar no WhatsApp
+            </a>
+        </div>
+    `;
 
-  return div;
+    return div;
 }
 
 imoveis.forEach(imovel => {
-  const card = criarCard(imovel);
+    const card = criarCard(imovel);
 
-  if (imovel.tipo === "venda") {
-    venda.appendChild(card);
-  } else {
-    aluguel.appendChild(card);
-  }
+    if (imovel.tipo === "venda") {
+        venda.appendChild(card);
+    }
+
+    if (imovel.tipo === "aluguel") {
+        aluguel.appendChild(card);
+    }
+
+    if (imovel.tipo === "temporada") {
+        temporada.appendChild(card);
+    }
 });
-function toggleMenu() {
-  document.querySelector(".navbar nav").classList.toggle("active");
-}

22c4defa3bbc690198d5549f2c46b6cfcf6df5b4
André Maia <andre.r.maia@gmail.com>
2026-05-05 12:23:29 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 const container = document.getElementById("imoveis");
 
-function renderizar(lista) {
+function renderizar() {
   container.innerHTML = "";
 
-  lista.forEach(imovel => {
+  imoveis.forEach(imovel => {
     container.innerHTML += `
       <div class="card">
-        <img src="${imovel.imagem}" alt="${imovel.titulo}">
+        <img src="${imovel.imagem}">
         <div class="card-content">
           <h3>${imovel.titulo}</h3>
           <p>${imovel.descricao}</p>
           <p><strong>R$ ${imovel.preco}</strong></p>
-          <a class="botao" target="_blank"
-            href="https://wa.me/5584999999999?text=Tenho interesse no imóvel: ${imovel.titulo}">
-            Falar no WhatsApp
+
+          <a class="botao saiba" href="#">Saiba mais</a>
+
+          <a class="botao whatsapp" target="_blank"
+          href="https://wa.me/5584999999999?text=Tenho interesse no imóvel ${imovel.titulo}">
+          Falar no WhatsApp
           </a>
         </div>
       </div>
@@ -21,13 +24,4 @@ function renderizar(lista) {
   });
 }
 
-function filtrar(tipo) {
-  if (tipo === "todos") {
-    renderizar(imoveis);
-  } else {
-    const filtrados = imoveis.filter(i => i.tipo === tipo);
-    renderizar(filtrados);
-  }
-}
-
-renderizar(imoveis);
+renderizar();

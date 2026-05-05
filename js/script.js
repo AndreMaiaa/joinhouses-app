d21f53287c709122831acb25fa7031a16a8aef0f
André Maia <andre.r.maia@gmail.com>
2026-05-05 14:00:02 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const vendaContainer = document.getElementById("venda");
-const aluguelContainer = document.getElementById("aluguel");
+const venda = document.getElementById("venda");
+const aluguel = document.getElementById("aluguel");
 
-function formatarPreco(valor) {
+function formatar(valor) {
   return valor.toLocaleString("pt-BR", {
     style: "currency",
     currency: "BRL"
@@ -17,32 +17,20 @@ function criarCard(imovel) {
     <div class="card-content">
       <h3>${imovel.titulo}</h3>
       <p>${imovel.descricao}</p>
-      <p><strong>${formatarPreco(imovel.preco)}</strong></p>
-      <a class="botao saiba" href="#">Saiba mais</a>
+      <p><strong>${formatar(imovel.preco)}</strong></p>
+      <a class="botao" href="#">Saiba mais</a>
     </div>
   `;
 
   return div;
 }
 
-function renderizar() {
-  vendaContainer.innerHTML = "";
-  aluguelContainer.innerHTML = "";
+imoveis.forEach(imovel => {
+  const card = criarCard(imovel);
 
-  imoveis.forEach(imovel => {
-    const card = criarCard(imovel);
-
-    if (imovel.tipo === "venda") {
-      vendaContainer.appendChild(card);
-    } else {
-      aluguelContainer.appendChild(card);
-    }
-  });
-}
-
-/* LOADER */
-window.addEventListener("load", () => {
-  document.getElementById("loader").style.display = "none";
+  if (imovel.tipo === "venda") {
+    venda.appendChild(card);
+  } else {
+    aluguel.appendChild(card);
+  }
 });
-
-renderizar();

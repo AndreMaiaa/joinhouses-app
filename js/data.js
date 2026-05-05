71da93567870ce226f282f9b5f29b3772172df0d
André Maia <andre.r.maia@gmail.com>
2026-05-05 12:17:14 -0300

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,18 @@
+const imoveis = [
+  {
+    id: 1,
+    titulo: "Apartamento em Natal",
+    preco: 250000,
+    tipo: "venda",
+    imagem: "https://via.placeholder.com/300",
+    descricao: "2 quartos, ótima localização"
+  },
+  {
+    id: 2,
+    titulo: "Casa para aluguel",
+    preco: 1200,
+    tipo: "aluguel",
+    imagem: "https://via.placeholder.com/300",
+    descricao: "3 quartos, garagem"
+  }
+];
